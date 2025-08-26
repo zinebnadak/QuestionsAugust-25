93f76bc2a72333d6ee8641aa4d3f523b0dc56c52
zinebnadak <zinebnadak04@outlook.com>
2025-08-26 14:27:23 +0300

    new notes 26.8

--- #3Questions.py
+++ #3Questions.py
@@ -1,1 +1,93 @@
+#TO CHOOSE
+
+
+#1 What sentence is used for choosing?
+#if-command
+
+#2 Write a program that tells you to turn on the heat if the temperature is under 18 degrees
+t = float (input("What is the current temperatur?:"))
+if t<18:
+    print("It is cold, turn on the heat!")
+
+print (f"It is {t:.1f} degrees")
+
+#3 If- sentence is a  "keyword", what else is a keyword?
+#import
+
+#4 how does if command work?
+# After if-command there needs to be a logical expression after, followed by a ":". Then INSIDE line that tells what to do if the expression is true. The lines of code except if and what is below will always run.
+
+#5 With if command we use many characters like "!=", with equal sign always on the right , what does this mean?
+# Not equal to
+
+#6 How do we express either this or this or this using if command, and...?
+# if, elif. You can use multiple Elif-parts. Keep in mind to begin the different parts equally in the line as the if command.
+
+#7How to express två alternatives
+# with else , is chosen if none of if or elif statements are true
+
+#8Can we add if commands inside if commands?
+#yes ofc, but remember to drag the if statement so it is "inside" the main if
+
+#9What is an logic expression, and what is the type of such?
+# Expression that is EITHER true or false, type is boo1, we are comparing and using comparing operators like =><! (or logical operators)
+
+#10 What more logical operators are used in logical expressions
+# and , or , not
+# A and B : true if both are true
+# A or B : true if at least one is true
+# not A : true if A is false
+
+#11 write a line of code using both comparing operators and logical operators
+# not (n==0 or n==100)
+
+#12 why can you combine  comparing operators, logical operators and arithmetic operators without using parenthesis
+# Because some operators have higher priority that others
+#1 First Arithmetical operators and comparing operators
+#2 Then Logical operators
+# i + j < k*k and m==n is same as (i+j)>(k*1) and (m==n)
+
+#13 What is speial with haraters "and" "or"
+# They are read from left to right, if first is false the second expression is never calculated. Used if you want to avoid to do following calculation if first statement is flase
+# k!=0 and
+
+#14 How can you write "1<=x and x<=5" ONLY in Python
+# 1 <= x <= 5
+
+#15 How can you create variables with this type boo1?
+# billig = True
+# barn = False
+# billig = pris <= 100
+# barn = ålder < 12
+
+#16 There are two cathegories of sentences in python
+# simple sentences and compound sentences. The compound sentences can consist of multiple sentences, ex. if/elif/else/for/while-sentences.
+
+#17 what does pass do?
+# adds below a command and does nothing
+
+#18 How can you write multiple sentences on the same line?
+# using semiolon ;
+# a=5 ; b=4 ; =7
+
+# 19 how to continue the same sentence on next row, without actually switching rows?
+# use backslash \ at the end of first row
+
+# 20 How can you write this code easier?
+# if x > y:
+#   z=x        the greater variable will get the name "z"
+# else:
+#   z=y
+#Answer: z = x if x>y else y
+
+#21 What is the act of dragging in code under a for example if-command called?
+# To indent
+
+#TO REPEAT
+
+
+
+
+
+
 
